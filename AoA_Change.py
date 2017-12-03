8370153b0c6dff439a306bbe88fd2843250aa8f5
pmparitakis <34171228+pmparitakis@users.noreply.github.com>
2017-12-03 21:52:35 +0200

    Python code

--- AoA_Change.py
+++ AoA_Change.py
@@ -2,6 +2,7 @@
 
 import math  
 
+# Function which calculates the coordinates of the airfoil at the new angle of attack.
 def rotate(origin, point, angle): 
   
     ox, oy = origin 
@@ -12,17 +13,19 @@ def rotate(origin, point, angle):
     
     return qx, qy
 
+# Open the .txt file that contains the airfoil coordinates at 0 angle of attack.
 coordsAt0 = [] 
 with open('coordinatesAt0.txt') as fin: 
     fin.readline() 
     for l in fin:     
         coords = map(float,l.split())
-        angle = 90 # degrees
-        origin = (0,0)
+        angle = 30 # The angle of attack the airfoil will be rotated COUNTER-CLOCKWISE!
+        origin = (0,0) # The origin where the airfoil will be rotated around.
         coords0 = rotate(origin, coords[:2], math.radians(angle))
         coordsAt0.append(coords0)
 
-with open('newcoordinates.geo','w') as fout:
+# Export the results into a ready-to-be-imported-into-gmsh .geo file.
+with open('newcoordinates.geo','w') as fout:  
     for i, c in enumerate(coordsAt0):
         fout.write('Point (%i) = {%f, %f, 0, 1};\n'%(i+1,c[0],c[1])) 
 
